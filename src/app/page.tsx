8c274816894f2e2e7b4d6b83ee212bf9a41ca5f1
Lakshya Sharma <106482122+lakshya2207@users.noreply.github.com>
2025-07-09 13:16:50 +0530

    mapping dropped images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,163 @@
+"use client";
+
+import React, { useState } from "react";
+import { IoShirtOutline } from "react-icons/io5";
+import { PiPants } from "react-icons/pi";
+import { TbShoe } from "react-icons/tb";
+import { BsHandbag } from "react-icons/bs";
+import { PiBaseballCap } from "react-icons/pi";
+import { RiGlassesFill } from "react-icons/ri";
+import { PiBelt } from "react-icons/pi";
+import { IoIosArrowRoundBack } from "react-icons/io";
+
 import Image from "next/image";
+type ImageCategories = {
+  accessories: string[];
+  tops: string[];
+  bottoms: string[];
+  shoes: string[];
+};
+type ImageCategoryKey = keyof ImageCategories;
 
-export default function Home() {
+const Page = () => {
+  const [images] = useState<ImageCategories>({
+    accessories: [
+      "/accessories/belt.jpg",
+      "/accessories/belt2.webp",
+      "/accessories/cap.png",
+      "/accessories/hat.png",
+      "/accessories/sunglasses.jpg",
+    ],
+    tops: [
+      "/tops/top1.webp",
+      "/tops/top2.jpg",
+      "/tops/top3.jpg",
+      "/tops/top4.png",
+      "/tops/top5.png",
+    ],
+    bottoms: [
+      "/bottoms/bottom1.webp",
+      "/bottoms/bottom2.webp",
+      "/bottoms/bottom3.jpg",
+      "/bottoms/bottom4.png",
+      "/bottoms/bottom5.png",
+    ],
+    shoes: [
+      "/shoes/shoes1.webp",
+      "/shoes/shoes2.webp",
+      "/shoes/shoes3.jpg",
+      "/shoes/shoes4.jpg",
+      "/shoes/shoes5.png",
+    ],
+  });
+  const [dragImage, setDragImage] = useState("");
+  const [selectedProp, setSelectedProp] = useState<ImageCategoryKey | "">("");
+  const [dragAreaImages, setDragAreaImages] = useState<string[]>([]);
+
+  const handleSelect = (prop: ImageCategoryKey | "") => {
+    setSelectedProp(prop);
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="main h-screen w-screen flex items-center justify-center flex-col gap-5 text-center">
+      <h1 className="font-bold text-4xl">Outfit Builder</h1>
+      <div className="box bg-gray-200 p-10 flex justify-between rounded-2xl shadow-xl gap-2 h-10/12 w-xl">
+        <div className="sidebar w-1/5 flex flex-col border-2 text-sm text-gray-700 font-semibold rounded-md border-gray-400">
+          {selectedProp === "" ? (
+            <>
+              <div
+                className="items h-1/4 m-1 p-5 flex flex-col items-center rounded-md bg-gray-300"
+                onClick={() => handleSelect("accessories")}
+              >
+                <div className="grid grid-cols-2 grid-rows-2 gap-2">
+                  <BsHandbag size={20} />
+                  <PiBaseballCap size={20} />
+                  <RiGlassesFill size={20} />
+                  <PiBelt size={20} />
+                </div>
+                Accessories
+              </div>
+              <div
+                className="items h-1/4 m-1 p-5 flex flex-col items-center rounded-md bg-gray-300"
+                onClick={() => handleSelect("tops")}
+              >
+                <IoShirtOutline size={35} />
+                Tops
+              </div>
+              <div
+                className="items h-1/4 m-1 p-5 flex flex-col items-center rounded-md bg-gray-300"
+                onClick={() => handleSelect("bottoms")}
+              >
+                <PiPants size={35} />
+                Bottom
+              </div>
+              <div
+                className="items h-1/4 m-1 p-5 flex flex-col items-center rounded-md bg-gray-300"
+                onClick={() => handleSelect("shoes")}
+              >
+                <TbShoe size={35} />
+                Shoes
+              </div>
+            </>
+          ) : (
+            <>
+              <IoIosArrowRoundBack
+                onClick={() => handleSelect("")}
+                size={30}
+                className="border-b-1 border-gray-400 z-10 w-full"
+              />
+              <div className="grig grid-cols-4 overflow-y-scroll gap-2">
+                {selectedProp &&
+                  selectedProp in images &&
+                  images[selectedProp as ImageCategoryKey].map((src, index) => (
+                    <Image
+                      key={index}
+                      src={src}
+                      alt={`${selectedProp} ${index + 1}`}
+                      width={50}
+                      height={50}
+                      className="object-contain h-1/4 mx-auto rounded bg-transparent bg-blend-multiply"
+                      onDragStart={() => setDragImage(src)}
+                    />
+                  ))}
+              </div>
+            </>
+          )}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="preview w-4/5 border-2 rounded-md border-gray-400">
+          <h3 className="font-semibold h-1/12 text-lg text-gray-700 mt-2">
+            Canvas Area
+          </h3>
+          <div
+            className="droparea mx-auto h-10/12 w-11/12 font-semibold text-gray-400 bg-slate-100 flex flex-wrap items-center justify-center border-dashed border-2 rounded-md p-4 gap-4"
+            onDragOver={(e) => e.preventDefault()} // Allow dropping
+            onDrop={(e) => {
+              e.preventDefault();
+              if (dragImage) {
+                setDragAreaImages((prev) => [...prev, dragImage]);
+                setDragImage("");
+              }
+            }}
+          >
+            {dragAreaImages.length > 0 ? (
+              dragAreaImages.map((src, index) => (
+                <Image
+                  key={index}
+                  src={src}
+                  alt={`Dropped item ${index + 1}`}
+                  width={80}
+                  height={80}
+                  className="object-contain"
+                />
+              ))
+            ) : (
+              <span>Drag & Drop the props here</span>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default Page;
